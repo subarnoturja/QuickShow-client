822ea38f7e4bc5aa84ac5db331533a494dd59a15
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-06-25 11:36:34 +0600

    router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-
+import { Route, Routes, useLocation } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Movies from "./pages/Movies"
+import MovieDetails from "./pages/MovieDetails"
+import SeatLayout from "./pages/SeatLayout"
+import MyBookings from "./pages/MyBookings"
+import Favorite from "./pages/Favorite"
+import Footer from "./components/Footer"
 
 function App() {
 
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
   return (
     <>
-      <div>
-        <h2>Hello World</h2>
-      </div>
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
