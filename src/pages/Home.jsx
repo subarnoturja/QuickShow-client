000b1831c6093cfe890505c4c75e1d913aa2745e
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-06-25 20:56:35 +0600

    trailer section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FeaturedSection from '../components/FeaturedSection';
 import HeroSection from '../components/HeroSection';
+import TrailersSection from '../components/TrailersSection';
 
 const Home = () => {
     return (
         <>
           <HeroSection />  
           <FeaturedSection />
+          <TrailersSection />
         </>
     );
 };
