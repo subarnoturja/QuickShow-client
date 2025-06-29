07a3547f29d845e37743ee632b2082433bf0e241
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-06-29 16:32:01 +0600

    featured section updated

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -1,13 +1,13 @@
 import { ArrowRight } from 'lucide-react'
-import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import BlurCircle from './BlurCircle';
-import { dummyShowsData } from '../assets/assets';
 import MovieCard from './MovieCard';
+import { useAppContext } from '../context/AppContext';
 
 const FeaturedSection = () => {
 
     const navigate = useNavigate();
+    const { shows } = useAppContext();
 
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
@@ -20,7 +20,7 @@ const FeaturedSection = () => {
         </div>
         {/* Movie Cards */}
         <div className='flex flex-wrap max-sm:justify-center gap-8 mt-8'>
-            {dummyShowsData.slice(0, 4).map((show) => (
+            {shows.slice(0, 4).map((show) => (
                 <MovieCard key={show._id} movie={show} />
             ))}
         </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,15 +2,17 @@ import { StarIcon } from 'lucide-react';
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import timeFormate from '../lib/timeFormate';
+import { useAppContext } from '../context/AppContext';
 
 const MovieCard = ({ movie }) => {
 
     const navigate = useNavigate();
+    const { image_base_url} = useAppContext()
 
   return (
     <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66'>
         <img onClick={() => {navigate(`/movies/${movie._id}`); scrollTo(0, 0)}}
-         src={movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer' />
+         src={image_base_url + movie.backdrop_path} alt="" className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer' />
          <p className='font-semibold mt-2 truncate'>{movie.title}</p>
          <p className='text-sm text-gray-400 mt-2'>
             {new Date(movie.release_date).getFullYear()}▫️{movie.genres.slice(0,2).map(genre => genre.name).join(" | ")}▫️{timeFormate(movie.runtime)}
